1a55ec362858b63741399dff947f47f607c88c56
Roger <rrobedo14@gmail.com>
2024-04-25 02:32:04 -0400

    url reset

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,11 +2,10 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-//const apiUrl = '/choreo-apis/djangoreacttutorial/backend/rest-api-be2/v1.0'
 
 const api = axios.create({
   // Loading enviorment variable
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
